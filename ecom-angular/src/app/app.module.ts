2a9e51672a1a5abd94effdcf8963133398780df4
Marouane LASMAK <lasmak2002@gmail.com>
2024-12-09 18:15:17 +0100

    v1.0

---
+++ ecom-angular/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './ui/products/products.component';
+import { OrdersComponent } from './ui/orders/orders.component';
+import { OrderDetailsComponent } from './ui/order-details/order-details.component';
+import { CustomersComponent } from './ui/customers/customers.component';
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'glsid-realm',
+        clientId: 'ecom-tp'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/silent-check-sso.html'
+      }
+    });
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    OrdersComponent,
+    OrderDetailsComponent,
+    CustomersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    KeycloakAngularModule,
+    HttpClientModule
+  ],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
